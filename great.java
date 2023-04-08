db76dbd001a0beb99349e125ef5b256d0efc033b
girishroyal695 <113019688+girishroyal695@users.noreply.github.com>
2023-04-08 09:27:45 +0530

    Create great.java

---
+++ great.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+class greater
+{
+    public static void main(String arg[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a,b;
+        a=sc.nextInt();
+        b=sc.nextInt();
+        if(a>b)
+        {
+            System.out.println("a is greater");
+        }
+        else
+        {
+            System.out.println("b is greater");
+        }
+    }
+}
